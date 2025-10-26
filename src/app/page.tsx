ed3d9dffd49f4013d7b835eb8f0625571c9ddbda
Meyoron Aghogho <youngmayor.dev@gmail.com>
2025-10-26 17:23:18 +0000

    Demo again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import MoodCanvas from '@/components/mood-canvas';
 export default function Home() {
   return (
     <main className="min-h-screen bg-background text-foreground overflow-hidden test">
-      <MoodCanvas />
+      {/* <MoodCanvas /> */}
+      I am Grace, a great software engineer
     </main>
   );
 }
